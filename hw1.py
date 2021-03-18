9f2f3d619444469c74a1f1fbf2a5a0f93a053d55
Jihee Ryu <chrisjihee@etri.re.kr>
2021-03-18 16:08:22 +0900

    Calculating Covariance Matrix

--- hw1.py
+++ hw1.py
@@ -17,20 +17,24 @@ def print_hi(name):
     print("-" * 120)
     print(f"target={iris['target']}")
     print("-" * 120)
+    X: np.ndarray = iris['data']
+    y: np.ndarray = iris['target']
 
     # Mean Vector (with Numpy)
-    mean_vector = iris['data'].mean(axis=0)
+    mean_vector = X.mean(axis=0)
     print(f"Mean Vector: [{', '.join('%.2f' % x for x in mean_vector)}]")
 
     # Mean Vector (without Numpy)
     sum_vector = [0.0, 0.0, 0.0, 0.0]
-    for x in iris['data']:
+    for x in X:
         sum_vector[0] += x[0]
         sum_vector[1] += x[1]
         sum_vector[2] += x[2]
         sum_vector[3] += x[3]
-    mean_vector = [x / len(iris['data']) for x in sum_vector]
+    mean_vector = [x / len(X) for x in sum_vector]
     print(f"Mean Vector: [{', '.join('%.2f' % x for x in mean_vector)}]")
+
+    # print(np.cov(X))
 
 
 if __name__ == '__main__':
