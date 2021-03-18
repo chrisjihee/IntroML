57a0441c7c63c4cef34fe2e97c88a3280195f8f3
Jihee Ryu <chrisjihee@etri.re.kr>
2021-03-18 15:18:44 +0900

    Load IRIS data

--- hw1.py
+++ hw1.py
@@ -1,5 +1,24 @@
+from sklearn import datasets
+from sklearn.utils import Bunch
+import numpy as np
+import matplotlib.pyplot as plt
+
+
 def print_hi(name):
-    print(f'Hi, {name}')
+    data: Bunch = datasets.load_iris()
+    print("-" * 120)
+    print(f"filename={data['filename']}")
+    print("-" * 120)
+    print(f"feature_names={data['feature_names']}")
+    print("-" * 120)
+    print(f"target_names={data['target_names']}")
+    print("-" * 120)
+    print(f"data={data['data']}")
+    print("-" * 120)
+    print(f"target={data['target']}")
+    print("-" * 120)
+    print(f"DESCR={data['DESCR']}")
+    print("-" * 120)
 
 
 if __name__ == '__main__':
